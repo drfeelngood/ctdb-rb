2b8f16f8fd5e3f181b39cc05711aaa73e71e192b
drfeelngood <dan@dj-agiledev.com>
2013-04-08 15:45:47 -0500

    Altered extconf file to allow for my dynamic installs

--- ext/ctdb_ext/extconf.rb
+++ ext/ctdb_ext/extconf.rb
@@ -13,42 +13,20 @@ def fail(msg)
   
 Try using the following options:
 
-  --with-faicom-dir=/path/to/faircom
+  --with-sdk-include=/path/to/faircom/include/sdk
+  --with-sdk-lib=/path/to/faircom/lib/sdk
 EOS
   printf("extconf.rb failure: %s\n", msg)
   exit
 end
 
-ct_dir   = with_config('faircom-dir')
+$CFLAGS << " -fPIC"
 
-api_dir  = case RUBY_PLATFORM
-when /darwin/ then %w(ctree.ctdb multithreaded static).join(File::SEPARATOR)
-when /linux/  then %w(ctree.ctdb multithreaded dynamic).join(File::SEPARATOR)
-else
-  puts "Unhandled platform `#{RUBY_PLATFORM}'"
-  exit 1
-end
-  
-lib      = "mtclient"
-lib_dir  = File.join(ct_dir, 'lib', api_dir)
-inc_dirs = [ File.join(ct_dir, 'include', 'sdk', api_dir),
-             File.join(ct_dir, 'include') ]
-
-inc_dirs.each do |path|
-  fail("FairCom path `#{path}' does not exist") unless File.exist?(path)
-  $INCFLAGS << " -I#{path}"
-end
-
-fail("FairCom path `#{lib_dir}' does not exist") unless File.exist?(lib_dir)
-$LIBPATH  << "#{lib_dir}"
-$CFLAGS   << " -fPIC"
-
-p $LIBPATH
-p $INCFLAGS
+dir_config("sdk")
 
 errors = []
 errors << "'ctdbsdk.h'" unless have_header('ctdbsdk.h')
-errors << "'#{lib}'"  unless have_library(lib, 'ctdbStringFormatToTime')
+errors << "'mtclient'"  unless have_library('mtclient', 'ctdbAllocSession')
 fail("missing dependencies: #{errors.join(',')}") unless errors.empty?
 
 create_makefile("ctdb_ext")
